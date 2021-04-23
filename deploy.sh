cae16009215a3f31a204f7fab0fcdc1d6787fee9
Hannah Gooden <hannah.gooden@jhuapl.edu>
2021-04-23 16:44:55 -0400

    Config changes

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:jhuapl-bio/barda.git master:gh-pages
+git push -f https://github.com/jhuapl-bio/BARDA.git master:gh-pages
 cd -
