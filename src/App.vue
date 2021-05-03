850cada264e60452b1b0c6e49ecaa21014b43da4
Hannah Gooden <hannah.gooden@jhuapl.edu>
2021-05-03 14:13:58 -0400

    Added apl blue, jhu shield, and footer

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,19 @@
 <template>
-  <div class="container">
+  <div>
+    <img class="navbar-image" src="@/assets/jhu-shield.png"/>
     <NavigationBar/>
-    <div class="columns pt-4">
-      <div class="column is-offset-1 is-10">
-        <router-view/>
+    <div class="router-outer container">
+      <div class="router-inner columns pt-4">
+        <div class="column is-offset-1 is-10">
+          <router-view/>
+        </div>
       </div>
     </div>
+    <footer class="footer">
+      <div class="content has-text-centered">
+        The work of JHU, JHU/APL, and BARDA. Not sure what content needs to legally be here.
+      </div>
+    </footer>
   </div>
 </template>
 
@@ -25,5 +33,23 @@ export default class App extends Vue {
 </script>
 
 <style scoped lang="scss">
-
+  .navbar-image {
+    max-height: 8rem;
+    padding: 1rem 9rem;
+  }
+  .router-outer {
+    display: flex;
+    // height of screen - relative height of header + footer
+    min-height: calc(100vh - 21.9rem);
+    flex-direction: column;
+  }
+  .router-inner {
+    flex: 1;
+  }
+  .footer {
+    height: 3rem;
+    width: 100%;
+    background-color: $aplgrey2;
+    // padding: 1rem;
+  }
 </style>
--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,15 +1,15 @@
 <template>
-  <b-navbar shadow>
+  <b-navbar type="is-primary">
     <template #brand>
-      <b-navbar-item class="navbar-item is-size-1 has-text-weight-semibold" :to="{path: '/'}">
-        BARDA
+      <b-navbar-item class="navbar-item is-size-1" :to="{path: '/'}">
+        Untitled Data Viz
       </b-navbar-item>
     </template>
     <template #end>
-        <b-navbar-item active class="is-size-4 has-text-weight-semibold">
+        <b-navbar-item class="is-size-4">
           <router-link to="/"> Data </router-link>
         </b-navbar-item>
-        <b-navbar-item class="is-size-4 has-text-weight-semibold">
+        <b-navbar-item class="is-size-4">
           <router-link to="/about"> About </router-link>
         </b-navbar-item>
     </template>
@@ -22,14 +22,23 @@ import { Component, Vue } from 'vue-property-decorator'
 
 @Component({})
 export default class NavigationBar extends Vue {
-
 }
 
 </script>
 
 <style scoped lang="scss">
   .navbar {
-    padding-left: 4em;
-    padding-right: 4em;
+    padding-left: 8rem;
+    padding-right: 8rem;
+  }
+  .navbar-item {
+    padding-bottom: 0rem;
+    padding-top: 0rem;
+  }
+  a {
+    color: white;
+  }
+  a:hover {
+    color: white;
   }
 </style>
