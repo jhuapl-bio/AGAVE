3cb73903798aa7674f99bb3e8c4ce1d61418df0a
Brian Merritt <Brian.Merritt@jhuapl.edu>
2021-05-07 14:07:03 -0400

    temp add sunburst

--- src/components/Visualization.vue
+++ src/components/Visualization.vue
@@ -1,12 +1,13 @@
 <template>
   <p>
     The visualization will go here
-  </p>
+  </p><Sunburst></Sunburst>
 </template>
 
 <script lang="ts">
 
 import { Component, Vue } from 'vue-property-decorator'
+import Sunburst from './Sunburst.vue'
 
 @Component({})
 export default class Visualization extends Vue {
