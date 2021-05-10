8a58c09c75777d5f4acbd214522a15852ade4cba
Hannah Gooden <hannah.gooden@jhuapl.edu>
2021-05-10 10:42:08 -0400

    Add module declaration

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,6 +1,8 @@
 declare module '*.vue' {
   import Vue from 'vue'
-  import * as d3 from 'd3'
-  import * as fs from 'file-system'
+  // import * as d3 from 'd3'
+  // import * as fs from 'file-system'
   export default Vue
 }
+
+declare module 'file-system';
