4c1928053fcad2a9965d898994b8b0a9be2324ff
Brian Merritt <brian.merritt@jhuapl.edu>
2021-07-20 14:33:08 -0400

    changing logo to be in navbar and to apl shield logo

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,26 +1,39 @@
 <template>
-  <div>
-    <div class=" pb-3 pt-3 left-padding">
-      <a href="https://jhu.edu">
-        <img class="navbar-image" src="@/assets/images/jhu-shield.png"/>
-      </a>
-    </div>
-    <nav class="navbar is-aplblue">
-      <div class="navbar-title">
+  <div style="">
+    <nav class="navbar is-aplblue" style="">
+      <div class="navbar-title" style="width: 100%">
         <div class="navbar-brand pl-3">
-          <h1 class="navbar-item is-size-2">
-            <!-- {{name.join(" ")}} -->
+          <div class="navbar-image is-size-2" style="float:right">
+            <a href="https://jhu.edu" >
+              <img class="navbar-image" src="@/assets/images/apl.shield.large.white.svg"/>
+            </a>
             
+          </div>
+          <h1 class="navbar-item is-size-2" style="float:left">
+            <!-- {{name.join(" ")}} -->
+            <div class="vertical">
+            </div>
             <span v-for="n in name"
             v-bind:key="n"
-            style="color: #fff;"
-            >
-            <p style="font-size: 1.2em; ">
-              <strong style="color: #fff;">{{n.substring(0,1)}}</strong><small style="font-size: 14px">{{n.substring(1,n.length)}}</small>
-            </p> 
-            
-
+            @mouseover="showTitleFull = true" @mouseleave="showTitleFull=false"
+            style="color: #fff; cursor: default">
+              {{n.substring(0,1)}}
+              <!-- <strong style="color: #fff;">{{n.substring(0,1)}}</strong><small style="font-size: 14px">{{n.substring(1,n.length)}}</small> -->
             </span>
+            <transition name="slide-fade">
+              <div style="display:flex" v-if="showTitleFull">
+                <div class = "vertical" >
+                </div>
+                <div >
+                  <span v-for="n in name"
+                    v-bind:key="n"
+                    style="color: #fff;">
+                    <small style="font-size: 14px">{{n}} </small>
+                  </span> 
+                </div>
+              </div>
+              
+            </transition> 
             <!-- <div style="display:block">
             <span v-for="n in name"
             v-bind:key="n"
@@ -44,13 +57,11 @@
             <hr>
             {{name.join("")}}
             </div> -->
-
-
-
           </h1>
         </div>
       </div>
     </nav>
+    
   </div>
 </template>
 
@@ -70,7 +81,9 @@ export default class NavigationBar extends Vue {
 <style scoped lang="scss">
   .navbar-image {
     max-height: 6rem;
-    padding-left: 6rem
+    padding-top: 0;
+    padding-bottom: 0;
+    // padding-left: 6rem;
   }
   .navbar-title {
     padding-left: 4rem;
@@ -85,6 +98,9 @@ export default class NavigationBar extends Vue {
   .navbar-burger {
     height: 4.5rem;
     width: 4.5rem;
+  }
+  nav {
+    display:block
   }
   a {
     color: white;
@@ -102,6 +118,24 @@ export default class NavigationBar extends Vue {
     transition: opacity .5s;
   }
   .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+    opacity: 0;
+  }
+  .vertical {
+      border-left: 2px solid #fff;
+      height: 100%;
+      padding-left: 10px;
+      margin-left: 10px;
+      vertical-align: baseline;
+  }
+  .slide-fade-enter-active {
+    transition: all .2s ease;
+  }
+  .slide-fade-leave-active {
+    transition: all .3s cubic-bezier(1.0, 0.5, 0.3, 0.8);
+  }
+  .slide-fade-enter, .slide-fade-leave-to
+  /* .slide-fade-leave-active below version 2.1.8 */ {
+    transform: translateX(10px);
     opacity: 0;
   }
 </style>
