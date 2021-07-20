b589571825ea8188ed74b652be76afd0df966983
Brian Merritt <brian.merritt@jhuapl.edu>
2021-07-20 11:33:40 -0400

    placeholder for agave

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -9,22 +9,46 @@
       <div class="navbar-title">
         <div class="navbar-brand pl-3">
           <h1 class="navbar-item is-size-2">
-            Influenza Variant Explorer
+            <!-- {{name.join(" ")}} -->
+            
+            <span v-for="n in name"
+            v-bind:key="n"
+            style="color: #fff;"
+            >
+            <p style="font-size: 1.2em; ">
+              <strong style="color: #fff;">{{n.substring(0,1)}}</strong><small style="font-size: 14px">{{n.substring(1,n.length)}}</small>
+            </p> 
+            
+
+            </span>
+            <!-- <div style="display:block">
+            <span v-for="n in name"
+            v-bind:key="n"
+            style="color: #fff; cursor: default"
+            @mouseover="showTitleFull = true" @mouseleave="showTitleFull=false"
+            >
+              <strong style="color: #fff">{{n.substring(0,1)}}
+              </strong>
+            </span>
+            <transition name="fade">
+              <p style="font-size: 12px" v-if="showTitleFull">{{name.join("")}}</p>
+            </transition>
+            </div> -->
+            <!-- <div style="display:block; font-size: 12px;">
+            <span v-for="n in name"
+            v-bind:key="n"
+            style="color: #fff; font-size: 29px; cursor: default"
+            >
+              {{n.substring(0,1)}}
+            </span>
+            <hr>
+            {{name.join("")}}
+            </div> -->
+
+
+
           </h1>
-          <!-- <a role="button" class="navbar-burger" :class="{'is-active': showMenu}" v-on:click="showMenu = !showMenu">
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-          </a> -->
         </div>
-        <!-- <div class="navbar-menu navbar-end bg-is-inherited" :class="{'is-active': showMenu}"> -->
-          <!-- <b-navbar-item class="is-size-4" :class="{'is-active': $route.name === 'Data'}">
-            <router-link to="/"> Data </router-link>
-          </b-navbar-item> -->
-          <!-- <b-navbar-item class="is-size-4" :class="{'is-active': $route.name === 'About'}">
-            <router-link to="/about"> About </router-link>
-          </b-navbar-item> -->
-        <!-- </div> -->
       </div>
     </nav>
   </div>
@@ -37,6 +61,8 @@ import { Component, Vue } from 'vue-property-decorator'
 @Component({})
 export default class NavigationBar extends Vue {
   private showMenu = false;
+  public showTitleFull = false;
+  public name: string[] = ['Automated', 'Genomics', 'Application for', 'Variant', 'Exploration']
 }
 
 </script>
@@ -71,5 +97,11 @@ export default class NavigationBar extends Vue {
   }
   a.navbar-item.is-active {
     background-color: rgba(0,0,0,0.25);
+  }
+  .fade-enter-active, .fade-leave-active {
+    transition: opacity .5s;
+  }
+  .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+    opacity: 0;
   }
 </style>
